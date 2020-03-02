41a2f99bc1d85a580cfb98c91300a33cba42f988
Varis-Patel <61026553+Varis-Patel@users.noreply.github.com>
2020-03-02 14:12:28 +0530

    Update employee_header.php

--- employee/employee_header.php
+++ employee/employee_header.php
@@ -45,7 +45,7 @@
             <div class="header-mobile__bar">
                 <div class="container-fluid">
                     <div class="header-mobile-inner">
-                        <a class="logo" href="admin_main.php">
+                        <a class="logo" href="employee_main.php">
                             <img src="../images/icon/logo.png" alt="CoolAdmin" />
                         </a>
                         <button class="hamburger hamburger--slider" type="button">
